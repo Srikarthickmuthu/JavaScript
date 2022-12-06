592f5b7aceee311afdcdc239a70d9f0e3b9589bf
srikarthickmuthu <srikarthick.muthu@aspiresys.com>
2022-12-06 16:36:44 +0530

    push

---
+++ mapAndFilter/index.js
@@ -0,0 +1,28 @@
+var array=[22,33,11,1,4,55];
+
+var array1=array.filter(age=>age>=18);
+
+console.log(array1);
+
+var array2=array.map(age=>{
+    if(age>=18){
+        return age;
+    }
+    else{
+        return false;
+    }
+});
+
+console.log(array2);
+
+var array3=array.forEach(age=>{
+    if(age>=18){
+        return age;
+    }
+    else{
+        return false;
+    }
+});
+
+console.log(array3);
+
